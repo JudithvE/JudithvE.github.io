4e2ee15ca954b31745e77f955841f36a0d162ab6
Enckevort <j.m.e.v.enckevort@student.tue.nl>
2020-04-15 15:48:40 +0200

    bla

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 $(() => {
-    $('#musicbutton').on('click', () => {
-        let sound = new Howl({ src: 'Pero_Asi.mp3' })      
-        sound.play()
-    })
+	let sound = new Howl({ src: 'Pero_Asi.mp3' })
+	let playpause = $('#musicplayer #playpause')      
+	playpause.on('click', () => {
+		playpause.toggleClass('paused') 
+		let paused = playpause.hasClass('paused')  
+		if(paused) {
+			playpause.attr('src', 'pause.png')
+			sound.play()
+		} else {
+			playpause.attr('src', 'play.png')
+			sound.pause()
+		}
+	})
+	setInterval(() => {
+		let dur = sound.duration()
+		let seek = sound.seek()
+		let a = (seek / dur)* 100
+		$('#bolletje').css('left', `${a}%`)
+		$('#progress').css('width', `${a}%`)
+	}, 100)
+
+	let draggingbolletje = false
+	let startdragx = 0
+	let startdragprogress = 0
+	$('#bolletje').on('mousedown', (e) => {
+		draggingbolletje = true
+		startdragx = e.clientX
+		console.log($('#bolletje')[0].style.left)
+		startdragprogress = Number($('#bolletje')[0].style.left.replace('%' , ''))
+
+	})
+	$('body').on('mouseup', () => { draggingbolletje = false})
+
+	$('body').on('mousemove', (e) => {
+		if(!draggingbolletje) return
+		let b = e.clientX-startdragx
+		let newprogress = startdragprogress + (b/Number($('#line').width()))*100
+		console.log(newprogress, startdragprogress, b, Number($('#line').width()))
+		sound.seek(sound.duration()*(newprogress/100))
+	})
+
 })
